2fc09f5e2d18de247ed6ca0d089d5609dde4f0ac
IanAvi15 <icaviles20@hotmail.com>
2026-03-29 23:05:26 -0400

    adding first program file

---
+++ firstprogram.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+
+    //My first C program
+    
+    /* This is also a comment 
+    with spaces*/
+    printf("I like pizza\n");
+    printf("It's really good\n");
+    return 0;
+}
